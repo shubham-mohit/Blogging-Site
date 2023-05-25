3fdfb1d37f273425c5660615e14c5657d861c5fd
shubham-mohit <shubhammohite801@gmail.com>
2023-05-25 15:23:23 +0530

    update done

--- src/Controller/blogsController.js
+++ src/Controller/blogsController.js
@@ -4,15 +4,20 @@ const ObjectId = require('mongoose').Types.ObjectId
 
 const blogs=async (req,res) => {
         let data = req.body
-        if(!data.authorId) {return res.send("Author Id not present")}
-        // else if(!ObjectId.isValid(data.authorId)) {return res.send("author id not valid")}
-        else{
-            let author = await authorModel.findById(data.authorId)
-            if(!author) {return res.send("author invalid")}
+        try{
+            if(!data.authorId) {return res.status(400).send("Author Id not present")}
+            // else if(!ObjectId.isValid(data.authorId)) {return res.send("author id not valid")}
             else{
-                let blogs = await blogsModel.create(data)
-                res.send({data:blogs})
+                let author = await authorModel.findById(data.authorId)
+                if(!author) {return res.status(400).send("author invalid")}
+                else{
+                    let blogs = await blogsModel.create(data)
+                    res.status(200).send({data:blogs})
+                }
             }
+        }
+        catch(error){
+            res.status(500).send(err)
         }
     }
     
