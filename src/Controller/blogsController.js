034e242a6b9a720994a1bf16ac9119ac2d918ccd
shubham-mohit <shubhammohite801@gmail.com>
2023-05-25 15:16:18 +0530

    create done

--- src/Controller/blogsController.js
+++ src/Controller/blogsController.js
@@ -3,29 +3,17 @@ const authorModel = require('../Models/authorModel')
 const ObjectId = require('mongoose').Types.ObjectId
 
 const blogs=async (req,res) => {
-    let data = req.body
-    // if(ObjectId.isValid(data.authorid._id)){
-    //     if(await authorModel.findById(data.authorid._id) ){
-    //     let CreatedBlogs  = await blogsModel.create(req.body)
-    //     return res.status(201).send({blogs :CreatedBlogs})
-    //     }
-    //     else{
-    //        return res.send("")
-    //     }
-    // }
-    // else{
-    //     return res.status(400).send("invalid request with a response body ")
-    // }
-    if(!ObjectId.isValid(data.authorId)){
-        return res.status(400).send("invalid request with a response body ")
-    }else{
-        if(await authorModel.findById(data.authorId) ){
-            let createdBlogs  = await blogsModel.create(req.body)
-            return res.status(201).send({blogs :createdBlogs})
-        }
+        let data = req.body
+        if(!data.authorId) {return res.send("Author Id not present")}
+        // else if(!ObjectId.isValid(data.authorId)) {return res.send("author id not valid")}
         else{
-            return res.send("")
+            let author = await authorModel.findById(data.authorId)
+            if(!author) {return res.send("author invalid")}
+            else{
+                let blogs = await blogsModel.create(data)
+                res.send({data:blogs})
+            }
         }
     }
     
-}
+module.exports.blogs = blogs
