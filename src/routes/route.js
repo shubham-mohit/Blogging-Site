031c413ca3744fc6f13a71b4fe19505b5a78eddf
Ketan Gupta <guptaketan6375@gmail.com>
2023-05-26 01:54:43 +0530

    some Error Message Change

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const router = express.Router()
-const {authors}=require('../Controller/authorController')
-const {blogs, getBlogs, update, blogDelete, deleteBlog}=require('../Controller/blogsController')
+const { authors } = require('../Controller/authorController')
+const { blogs, getBlogs, update, blogDelete, deleteBlog } = require('../Controller/blogsController')
 
 router.post('/authors', authors)
 router.post('/blogs', blogs)
-router.put("/update/:blogId" , update)
+router.put("/update/:blogId", update)
 router.get('/blogs', getBlogs)
-router.delete('/blogs/:blogId',blogDelete )
-router.delete('/blogs',deleteBlog )
+router.delete('/blogs/:blogId', blogDelete)
+router.delete('/blogs', deleteBlog)
 
 module.exports = router
