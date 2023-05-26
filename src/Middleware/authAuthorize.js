811a37648a57b5ef8c81fb1fcd55c7774daef523
https://github.com/rahul-6809 <rkpandeyisin2015@gmail.com>
2023-05-26 13:13:52 +0530

    authrization

---
+++ src/Middleware/authAuthorize.js
@@ -0,0 +1,24 @@
+const jwt = require("jsonwebtoken");
+const { SECRETE_KEY } = require("../../config");
+
+const authorization = (req, res, next) => {
+  try {
+    if(!req.headers) return res.status(400).send({status:false, message:"Please provide headers."});
+    else {
+        const token = req.headers[`x-api-key`]
+        if(!token) return res.status(400).send({status:false, message:"Please provide token."});
+        else {
+          const DecodeToken= jwt.verify(token,SECRETE_KEY)
+          if(!DecodeToken) return res.status(400).send({status:false, message:"Invalid token."});
+          else{
+            req.authorId=DecodeToken.userId;
+            next();
+          }
+    }
+  } }
+  catch (err) {
+    res.status(403).send({status:false,message:err.message});
+  }
+};
+
+module.exports = authorization;
--- src/routes/route.js
+++ src/routes/route.js
@@ -2,13 +2,15 @@ const express = require('express')
 const router = express.Router()
 const { authors, login } = require('../Controller/authorController')
 const { blogs, getBlogs, update, blogDelete, deleteBlog } = require('../Controller/blogsController')
+const authorization = require('../Middleware/authAuthorize')
 
 router.post('/authors', authors)
 router.post('/blogs', blogs)
 router.put("/update/:blogId", update)
-router.get('/blogs', getBlogs)
+router.get('/blogs',authorization, getBlogs)
 router.delete('/blogs/:blogId', blogDelete)
 router.delete('/blogs', deleteBlog)
 router.post("/login", login)
+
 
 module.exports = router
